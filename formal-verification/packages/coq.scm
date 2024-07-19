518013b0dd2aab40128a9c6e11167c3a96b3c793
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2024-07-19 13:38:32 +0200

    formal-verification: Add coq-regexp.
    * formal-verification/packages/coq.scm (coq-regexp): New variable.

--- formal-verification/packages/coq.scm
+++ formal-verification/packages/coq.scm
@@ -2,6 +2,7 @@
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
 
 (define-module (formal-verification packages coq)
+  #:use-module (gnu packages)
   #:use-module (gnu packages coq)
   #:use-module (gnu packages ocaml)
   #:use-module (guix build-system gnu)
@@ -30,3 +31,38 @@
     (native-inputs (list coq))
     (propagated-inputs (list))
     (inputs (list))))
+
+(define-public coq-regexp
+  (let ((revision "0")
+        (commit "da6d250506ea667266282cc7280355d13b27c68d"))
+    (package
+      (name "coq-regexp")
+      (version (git-version "0.0.0" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                       (url "https://github.com/coq-contribs/regexp")
+                       (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "1s1rxrz6yq8j0ykd2ra0g9mj8ky2dvgim2ysjdn5yz514b36mc7x"))
+                (patches
+                 (search-patches
+                  "patches/coq-regex-coqlex-additions.patch"))))
+      (build-system gnu-build-system)
+      (arguments
+       (list #:make-flags
+             #~(list (string-append "COQLIBINSTALL=" #$output
+                     "/lib/coq/user-contrib"))
+             #:tests? #f
+             #:phases
+             #~(modify-phases %standard-phases
+                 (delete 'configure))))
+      (native-inputs (list coq))
+      (home-page "https://github.com/coq-contribs/regexp")
+      (synopsis "")
+      (description "")
+      ;; TODO: License says on file://description that it is LGPL, however
+      ;; version is not specified.
+      (license license:lgpl3))))
