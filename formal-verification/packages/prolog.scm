4e60363c2ade1ac75de24bc82680c5c455f85fd9
Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
2024-08-19 11:08:56 +0200

    formal-verification: Add elpi.
    * formal-verification/packages/prolog.scm (elpi): New variable.

---
+++ formal-verification/packages/prolog.scm
@@ -0,0 +1,46 @@
+;;; SPDX-FileCopyrightText: © 2024 Jean-Pierre De Jesus DIAZ <me@jeandudey.tech>
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+
+(define-module (formal-verification packages prolog)
+  #:use-module (formal-verification packages serialization)
+  #:use-module (gnu packages ocaml)
+  #:use-module (guix build-system dune)
+  ;#:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages))
+
+(define-public elpi
+  (package
+    (name "elpi")
+    (version "1.19.5")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                     (url "https://github.com/LPCIC/elpi")
+                     (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "1azb6j9kki8i8q30dpx39nrfjvfmdgq92wr69wybssijdmzxcy07"))))
+    (build-system dune-build-system)
+    ;(arguments
+    ; (list #:phases
+    ;       #~(modify-phases %standard-phases
+    ;           (delete 'configure))))
+    (propagated-inputs (list ocaml-re ocaml-yojson))
+    
+    (inputs
+     (list ocaml-easy-format     ; Propagate on ocaml-biniou.
+           ocaml-camlp-streams)) ; Likewise.
+    (native-inputs (list atd ocaml-ppx-deriving ocaml-menhir))
+    (home-page "https://github.com/LPCIC/elpi")
+    (synopsis "Embeddable λProlog interpreter")
+    (description "This package provides the @acronym{ELPI, Embeddable Lambda
+Prolog Interpreter} that can be embedded into applications written in the
+OCaml programming language.  It comes with an @acronym{API, Application
+Programming Interface} for the interpreter and with a @acronym{FFI, Foreign
+Function Interface} for defining built-in predicates and data types, as well
+as quotations and similar interfaces that are useful to adapt the language to
+the desired application.")
+    (license license:lgpl2.1+)))
